5ab3a1cdc8e4e177acc40e6fdd8c4882b991c72b
FarXY29 <hambali1129@gmail.com>
2024-06-05 13:31:13 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,46 @@
+<!-- <?php include "header.php" ?> -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center ">
+            <img src="img/IMG_5194 copy.jpg" alt="Muhammad Hambali" width="150" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK :</strong></td>
+                        <td align="left" width="50%">2210010154 </td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Hambali Ta'simbillah</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Barabai, 29 November 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">085155488308</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">hambali1129@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://instagram.com/hambali1129" target="_blank" style="text-decoration:none;">
+                                <img src="img/icons8-instagram-96.png" width="30" alt="instagram">
+                            </a>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
---
+++ test.php
@@ -0,0 +1,2 @@
+<?php
+echo password_hash("hambali", PASSWORD_DEFAULT);
